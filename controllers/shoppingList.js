415f384af4bf6cb9338642fedd4efb27b9f82881
Shane Linden <shanelinden1995@gmail.com>
2020-01-14 10:45:09 +0200

    Connecting the controller

---
+++ controllers/shoppingList.js
@@ -0,0 +1,3 @@
+exports.getShoppingList = (req, res, next) => {
+	res.render('shopping-list')
+}
--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,9 +1,9 @@
 const express = require('express')
 
+const shoppingListController = require('../controllers/shoppingList')
+
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-	res.render('shopping-list')
-})
+router.get('/', shoppingListController.getShoppingList)
 
 module.exports = router
