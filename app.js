07288ab2e41492e4289168bbfa9ee24a90b58718
Shane Linden <shanelinden1995@gmail.com>
2020-01-14 10:55:27 +0200

    Added MongoDB with Mongoose

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const connect = require('mongoose').connect
 
 const rootDir = require('./util/path')
 
 const shoppingListRoutes = require('./routes/shoppingList')
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(rootDir, 'public')))
-
 app.use(shoppingListRoutes)
 
-app.listen(3000)
+connect(
+	'mongodb+srv://shane_01:ShaneLinden1@cluster0-tcqav.mongodb.net/shop?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true
+	}
+).then(() => {
+	app.listen(3000, () => {
+		console.log(`Connected on port ${PORT}`)
+	})
+})
